149957ec52d71f70f3299809d2a28c4e9c074c58
ahmerhabib <60505302+ahmerhh@users.noreply.github.com>
2023-08-02 00:24:09 +0100

    Update test/index.js
    Adding comments

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ import test from 'tape';
 import isInteger from 'is-integer';
 import * as utils from '../src/';
 
+// Test getRandomValueInRange utility function
 test('getRandomValueInRange', ({ test }) => {
   test('should returns a random value within a min and max value', t => {
     t.plan(1);
@@ -11,16 +12,16 @@ test('getRandomValueInRange', ({ test }) => {
     const min = -Math.random() * 100;
     const max = Math.random() * 100;
 
-    for(let i = 0; i < 100; i++) {
+    for (let i = 0; i < 100; i++) {
       const value = getRandomValueInRange(min, max);
-      if(value < min || value > max) {
+      if (value < min || value > max) {
         return t.fail(`${value} is outside of [${min}, ${max}] range`);
       }
     }
 
     t.pass('ok');
   });
-  
+
   test('should returns a random rounded value within a min and max value', t => {
     t.plan(1);
 
@@ -29,14 +30,14 @@ test('getRandomValueInRange', ({ test }) => {
     const min = Math.round(-Math.random() * 100);
     const max = Math.round(Math.random() * 100);
 
-    for(let i = 0; i < 100; i++) {
+    for (let i = 0; i < 100; i++) {
       const value = getRandomValueInRange(min, max, true);
 
-      if(!isInteger(value)) {
+      if (!isInteger(value)) {
         return t.fail(`${value} is not an integer`);
       }
 
-      if(value < min || value > max) {
+      if (value < min || value > max) {
         return t.fail(`${value} is outside of [${min}, ${max}] range`);
       }
     }
@@ -45,15 +46,16 @@ test('getRandomValueInRange', ({ test }) => {
   });
 });
 
+// Test mapValueToRange utility function
 test('mapValueToRange', ({ test }) => {
   test('should map a value from one range to another', t => {
     t.plan(1);
 
     const { mapValueToRange } = utils;
 
-    for(let i = 0; i < 100; i++) {
+    for (let i = 0; i < 100; i++) {
       const val = mapValueToRange(i, 0, 100, 0, 1);
-      if(val !== i / 100) {
+      if (val !== i / 100) {
         return t.fail();
       }
     }
@@ -62,6 +64,7 @@ test('mapValueToRange', ({ test }) => {
   });
 });
 
+// Test isPowerOfTwo utility function
 test('isPowerOfTwo', ({ test }) => {
   test('should tell if a number if a power of two', t => {
     t.plan(1);
@@ -70,19 +73,20 @@ test('isPowerOfTwo', ({ test }) => {
 
     const multiples = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096];
 
-    for(let i = 0; i < 4097; i++) {
-      const value = isPowerOfTwo(i); 
+    for (let i = 0; i < 4097; i++) {
+      const value = isPowerOfTwo(i);
       const expected = multiples.indexOf(i) === -1 ? false : true;
 
-      if(value !== expected) {
+      if (value !== expected) {
         return t.fail(`${i}, expected: ${expected}, value: ${value}`);
       }
     }
 
     t.pass('ok');
-  })
+  });
 });
 
+// Test roundToMultiple utility function
 test('roundToMultiple', ({ test }) => {
   test('should round a number to a given multiple', t => {
     const { roundToMultiple } = utils;
